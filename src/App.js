df64bb223f3a019f282ed3c3ce570810ed662048
OndigoWendy <ondigowendy004@gmail.com>
2022-09-27 12:15:55 +0300

    Enable controls

--- src/App.js
+++ src/App.js
@@ -8,74 +8,73 @@ import {
   Input,
   SkeletonText,
   Text,
-} from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes } from 'react-icons/fa'
-import {
-  useJsApiLoader,
-  GoogleMap
-} from '@react-google-maps/api'
-
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes } from "react-icons/fa";
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
 
 function App() {
-
   //Hook
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries: ['places']
-  })
+    libraries: ["places"],
+  });
 
   if (!isLoaded) {
-    return <SkeletonText />
+    return <SkeletonText />;
   }
-  const center = { lat: -1.3020, lng: 36.8222 }
+  const center = { lat: -1.302, lng: 36.8222 };
 
   return (
     <Flex
-      position='relative'
-      flexDirection='column'
-      alignItems='center'
-      h='100vh'
-      w='100vw'
+      position="relative"
+      flexDirection="column"
+      alignItems="center"
+      h="100vh"
+      w="100vw"
     >
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
-      {/* Google Map Box */}
-      <GoogleMap
-      center={center}
-      zoom={3}
-      mapContainerStyle={{ width: '100%', height: '100%' }}
->
-          
-        </GoogleMap>
+      <Box position="absolute" left={0} top={0} h="100%" w="100%">
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={3}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          options={{
+            zoomControl: true,
+            streetViewControl: true,
+            mapTypeControl: true,
+            fullscreenControl: true,
+          }}
+        ></GoogleMap>
       </Box>
-        
+
       <Box
         p={4}
-        borderRadius='lg'
+        borderRadius="lg"
         mt={4}
-        bgColor='white'
-        shadow='base'
-        minW='container.md'
-        zIndex='modal'
+        bgColor="white"
+        shadow="base"
+        minW="container.md"
+        zIndex="1"
       >
         <HStack spacing={4}>
-          <Input type='text' placeholder='Origin' />
-          <Input type='text' placeholder='Destination' />
+          <Input type="text" placeholder="Origin" />
+          <Input type="text" placeholder="Destination" />
           <ButtonGroup>
-            <Button colorScheme='pink' type='submit'>
+            <Button colorScheme="pink" type="submit">
               Calculate Route
             </Button>
             <IconButton
-              aria-label='center back'
+              aria-label="center back"
               icon={<FaTimes />}
               onClick={() => alert(123)}
             />
           </ButtonGroup>
         </HStack>
-        <HStack spacing={4} mt={4} justifyContent='space-between'>
+        <HStack spacing={4} mt={4} justifyContent="space-between">
           <Text>Distance: </Text>
           <Text>Duration: </Text>
           <IconButton
-            aria-label='center back'
+            aria-label="center back"
             icon={<FaLocationArrow />}
             isRound
             onClick={() => alert(123)}
@@ -83,7 +82,7 @@ function App() {
         </HStack>
       </Box>
     </Flex>
-  )
+  );
 }
 
-export default App
+export default App;
